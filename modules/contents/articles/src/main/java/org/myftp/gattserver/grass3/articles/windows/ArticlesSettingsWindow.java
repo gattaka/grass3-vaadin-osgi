4b8011f8f299ecd7161c88136c37fe1db3ce1105
gattaka <gattakahynca@seznam.cz>
2013-01-30 20:57:34 +0000

    Reprocessing with progessbar implemented

--- modules/contents/articles/src/main/java/org/myftp/gattserver/grass3/articles/windows/ArticlesSettingsWindow.java
+++ modules/contents/articles/src/main/java/org/myftp/gattserver/grass3/articles/windows/ArticlesSettingsWindow.java
@@ -12,15 +12,17 @@ import org.myftp.gattserver.grass3.config.ConfigurationManager;
 import org.myftp.gattserver.grass3.config.ConfigurationUtils;
 import org.myftp.gattserver.grass3.facades.ContentTagFacade;
 import org.myftp.gattserver.grass3.subwindows.ConfirmSubwindow;
-import org.myftp.gattserver.grass3.subwindows.GrassSubWindow;
 import org.myftp.gattserver.grass3.windows.template.SettingsWindow;
 
 import com.vaadin.data.validator.AbstractStringValidator;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.ProgressIndicator;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Window;
 
 public class ArticlesSettingsWindow extends SettingsWindow {
 
@@ -30,6 +32,12 @@ public class ArticlesSettingsWindow extends SettingsWindow {
 	private ContentTagFacade contentTagFacade = ContentTagFacade.INSTANCE;
 
 	private VerticalLayout settingsLayout = new VerticalLayout();
+
+	private Window progressSubWindow;
+	private ProgressThread progressThread;
+	private ProgressIndicator progressbar;
+	private Label progressItemLabel;
+	private Button reprocessButton;
 
 	/**
 	 * Validátor pro validaci kladný celých čísel (celá čísla větší než nula)
@@ -160,53 +168,136 @@ public class ArticlesSettingsWindow extends SettingsWindow {
 		reprocessLayout.setSpacing(true);
 		reprocessLayout.setSizeFull();
 
-		Button reprocessButton = new Button("Přegenerovat všechny články",
-				new Button.ClickListener() {
+		reprocessButton = new Button("Přegenerovat všechny články");
+		reprocessButton.addListener(new Button.ClickListener() {
 
-					private static final long serialVersionUID = 8490964871266821307L;
+			private static final long serialVersionUID = 8490964871266821307L;
 
-					public void buttonClick(ClickEvent event) {
+			public void buttonClick(ClickEvent event) {
 
-						addWindow(new ConfirmSubwindow(
-								"Přegenerování všech článků může zabrat delší čas a dojde během něj zřejmě k mnoha drobným změnám - opravdu přegenerovat ?") {
+				ConfirmSubwindow confirmSubwindow = new ConfirmSubwindow(
+						"Přegenerování všech článků může zabrat delší čas a dojde během něj zřejmě k mnoha drobným změnám - opravdu přegenerovat ?") {
 
-							private static final long serialVersionUID = -1214461419119865670L;
+					private static final long serialVersionUID = -1214461419119865670L;
 
-							@Override
-							protected void onConfirm(ClickEvent event) {
+					@Override
+					protected void onConfirm(ClickEvent event) {
 
-								// GrassSubWindow processSubWindow = new
-								// GrassSubWindow(
-								// "Průbeh operace");
-								// addWindow(processSubWindow);
-
-								// tohle se musí hodit do vlákna dle
-								// http://demo.vaadin.com/sampler#ProgressIndicators
-
-								List<ArticleDTO> articles = articleFacade
-										.getAllArticles();
-
-								for (ArticleDTO article : articles) {
-
-									String tags = contentTagFacade
-											.serializeTags(article
-													.getContentNode()
-													.getContentTags());
-									articleFacade.modifyArticle(article
-											.getContentNode().getName(),
-											article.getText(), tags, article
-													.getContentNode()
-													.getPublicated(), article);
-
-								}
-
-							}
-						});
+						progressSubWindow = new ProgressSubWindow();
+						getParent().addWindow(progressSubWindow);
 
 					}
-				});
+				};
+				confirmSubwindow.setWidth("460px");
+				confirmSubwindow.setHeight("230px");
+				addWindow(confirmSubwindow);
+
+			}
+		});
 		reprocessLayout.addComponent(reprocessButton);
 
 		super.onShow();
+	}
+
+	/**
+	 * Je volán vláknem aby se aktualizoval stav progressbaru
+	 */
+	public void prosessed() {
+		float progress = progressThread.getProgress();
+		String progressItemName = progressThread.getCurrentName();
+		progressbar.setValue(progress);
+		progressItemLabel.setValue(progressItemName);
+		if (progress == 1) {
+			progressbar.setEnabled(false);
+			reprocessButton.setEnabled(true);
+			progressSubWindow.getParent().removeWindow(progressSubWindow);
+		}
+	}
+
+	public class ProgressThread extends Thread {
+
+		private int total;
+		private int current;
+		private String currentName;
+
+		@Override
+		public void run() {
+
+			List<ArticleDTO> articles = articleFacade.getAllArticles();
+			total = articles.size();
+			current = 0;
+
+			for (ArticleDTO article : articles) {
+
+				String tags = contentTagFacade.serializeTags(article
+						.getContentNode().getContentTags());
+				articleFacade.modifyArticle(article.getContentNode().getName(),
+						article.getText(), tags, article.getContentNode()
+								.getPublicated(), article);
+
+				synchronized (getApplication()) {
+					current++;
+					currentName = article.getContentNode().getName();
+					prosessed(); // aktualizuj stav progressbaru
+				}
+			}
+		}
+
+		/**
+		 * @return procentuální stav hotové práce
+		 */
+		public float getProgress() {
+			return (float) current / total;
+		}
+		
+		public String getCurrentName() {
+			return currentName;
+		}
+
+	}
+	
+	public class ProgressSubWindow extends Window {
+
+		private static final long serialVersionUID = 2717898756701926156L;
+
+		public ProgressSubWindow() {
+			super("Průběh operace");
+
+			setWidth("300px");
+			setHeight("170px");
+			center();
+
+			// okno se nesmí dát zavřít 
+			setReadOnly(true);
+			
+			VerticalLayout processWindowLayout = new VerticalLayout();
+			setContent(processWindowLayout);
+			
+			processWindowLayout.setMargin(true);
+			processWindowLayout.setSpacing(true);
+			processWindowLayout.setSizeFull();
+			
+			progressbar = new ProgressIndicator();
+			progressbar.setIndeterminate(false);
+			progressbar.setEnabled(false);
+			processWindowLayout.addComponent(progressbar);
+			processWindowLayout.setComponentAlignment(progressbar,
+					Alignment.MIDDLE_CENTER);
+
+			progressItemLabel = new Label("");
+			processWindowLayout.addComponent(progressItemLabel);
+			processWindowLayout.setComponentAlignment(progressItemLabel,
+					Alignment.MIDDLE_CENTER);
+			
+			// aby to nešlo pustit dvakrát vedle sebe
+			reprocessButton.setEnabled(false);
+
+			progressThread = new ProgressThread();
+			progressThread.start();
+			progressbar.setEnabled(true);
+			progressbar.setValue(0f);
+		
+		}
+		
 	}
 }

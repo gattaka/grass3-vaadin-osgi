8d69bf3a28441ebd52436512a9fd236177b2b52c
gattaka <gattakahynca@seznam.cz>
2017-01-17 19:16:42 +0100

    Recipes java 8 test

--- modules/recipes/src/main/java/cz/gattserver/grass3/recipes/ui/MainUI.java
+++ modules/recipes/src/main/java/cz/gattserver/grass3/recipes/ui/MainUI.java
@@ -1,12 +1,8 @@
 package cz.gattserver.grass3.recipes.ui;
 
-import javax.servlet.http.HttpServletRequest;
-
-import cz.gattserver.grass3.wexp.DispatchAction;
 import cz.gattserver.grass3.wexp.Request;
 import cz.gattserver.grass3.wexp.in.impl.Label;
 import cz.gattserver.grass3.wexp.in.impl.Link;
-import cz.gattserver.grass3.wexp.in.impl.UI;
 
 public class MainUI extends AbstractUI {
 
@@ -23,14 +19,11 @@ public class MainUI extends AbstractUI {
 		listLink.setCSSClass("menu-item");
 
 		Link createLink;
-		layout.addChild(createLink = new Link("založit recept", new DispatchAction() {
-			private static final long serialVersionUID = 5853456653676352799L;
 
-			@Override
-			public UI dispatch(HttpServletRequest req) {
-				return new CreateUI(MainUI.this);
-			}
+		layout.addChild(createLink = new Link("založit recept", req -> {
+			return new CreateUI(MainUI.this);
 		}));
+
 		createLink.setCSSClass("menu-item");
 
 	}

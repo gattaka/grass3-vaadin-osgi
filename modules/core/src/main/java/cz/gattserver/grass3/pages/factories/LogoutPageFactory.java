ed89dd1232e9d200a77afee948fa2cf0a4976332
gattaka <gattakahynca@seznam.cz>
2017-10-16 20:28:00 +0200

    Logout fixed; Login security changed to BCryptPasswordEncoder

--- modules/core/src/main/java/cz/gattserver/grass3/pages/factories/LogoutPageFactory.java
+++ modules/core/src/main/java/cz/gattserver/grass3/pages/factories/LogoutPageFactory.java
@@ -1,22 +1,18 @@
 package cz.gattserver.grass3.pages.factories;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
 import com.vaadin.server.Page;
-import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServletRequest;
 import com.vaadin.server.VaadinServletResponse;
-import com.vaadin.server.VaadinSession;
 
 import cz.gattserver.grass3.pages.LoginPage;
 import cz.gattserver.grass3.pages.factories.template.AbstractPageFactory;
 import cz.gattserver.grass3.pages.template.GrassPage;
-import cz.gattserver.grass3.security.CoreACL;
 import cz.gattserver.grass3.ui.util.GrassRequest;
 
 @Component("logoutPageFactory")
@@ -24,31 +20,24 @@ public class LogoutPageFactory extends AbstractPageFactory {
 
 	private static final long serialVersionUID = 259563676017857878L;
 
-	@Autowired
-	private CoreACL coreACL;
-
 	public LogoutPageFactory() {
 		super("logoutpage");
 	}
 
 	@Override
 	protected boolean isAuthorized() {
-		// lze pouze je-li přihlášen
-		return coreACL.isLoggedIn(getUser());
+		return true;
 	}
 
 	@Override
 	protected GrassPage createPage(GrassRequest request) {
-		VaadinSession.getCurrent().getSession().invalidate();
-		VaadinSession.getCurrent().close();
-		VaadinService.reinitializeSession(VaadinService.getCurrentRequest());
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (auth != null) {
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			Page.getCurrent().reload();
 			new SecurityContextLogoutHandler().logout(VaadinServletRequest.getCurrent(),
 					VaadinServletResponse.getCurrent(), auth);
+			SecurityContextHolder.clearContext();
 		}
-		// context.setAuthentication(null);
-		SecurityContextHolder.clearContext();
 
 		return new LoginPage(request);
 	}

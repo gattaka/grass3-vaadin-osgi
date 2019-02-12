c352387642d071421533993f4d53958742aed08d
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-02-12 22:47:57 +0100

    Link caption shortening

--- modules/contents/plugins/favlink/src/main/java/cz/gattserver/grass3/articles/plugins/favlink/plugin/FavlinkElement.java
+++ modules/contents/plugins/favlink/src/main/java/cz/gattserver/grass3/articles/plugins/favlink/plugin/FavlinkElement.java
@@ -5,6 +5,8 @@ import cz.gattserver.grass3.articles.editor.parser.elements.Element;
 
 public class FavlinkElement implements Element {
 
+	private static final int MAX_LENGTH = 100;
+
 	private String link = null;
 	private String imgURL = null;
 
@@ -13,12 +15,18 @@ public class FavlinkElement implements Element {
 		this.link = pageURL;
 	}
 
+	private String createShortLink(String link) {
+		if (link.length() <= MAX_LENGTH)
+			return link;
+		return link.substring(0, MAX_LENGTH / 2 - 3) + "..." + link.substring(link.length() - MAX_LENGTH / 2);
+	}
+
 	@Override
 	public void apply(Context ctx) {
 		ctx.print("<a style=\"word-wrap: break-word\" href=\"" + link + "\" >");
 		if (imgURL != null)
 			ctx.print("<img style=\"margin: 4px 5px -4px 2px;\" height=\"16\" width=\"16\" src=\"" + imgURL + "\" />");
-		ctx.print(link);
+		ctx.print(createShortLink(link));
 		ctx.print("</a>");
 	}
 }
--- modules/contents/plugins/favlink/src/test/java/cz/gattserver/grass3/articles/plugins/favlink/plugin/FavlinkElementTest.java
+++ modules/contents/plugins/favlink/src/test/java/cz/gattserver/grass3/articles/plugins/favlink/plugin/FavlinkElementTest.java
@@ -21,11 +21,24 @@ public class FavlinkElementTest {
 	}
 
 	@Test
-	public void test_empty() {
+	public void testEmpty() {
 		FavlinkElement element = new FavlinkElement(null, "http://mock.url.website");
 		Context context = new ContextImpl();
 		element.apply(context);
-		assertEquals("<a style=\"word-wrap: break-word\" href=\"http://mock.url.website\" >http://mock.url.website</a>", context.getOutput());
+		assertEquals("<a style=\"word-wrap: break-word\" href=\"http://mock.url.website\" >http://mock.url.website</a>",
+				context.getOutput());
+	}
+
+	@Test
+	public void testLong() {
+		FavlinkElement element = new FavlinkElement("mock/imgs/favicon/fav.ico",
+				"http://mock.url.website/questions/3357477/is-asynctask-really-conceptually-flawed-or-am-i-just-missing-something/3359003#3359003");
+		Context context = new ContextImpl();
+		element.apply(context);
+		assertEquals(
+				"<a style=\"word-wrap: break-word\" href=\"http://mock.url.website/questions/3357477/is-asynctask-really-conceptually-flawed-or-am-i-just-missing-something/3359003#3359003\" >"
+						+ "<img style=\"margin: 4px 5px -4px 2px;\" height=\"16\" width=\"16\" src=\"mock/imgs/favicon/fav.ico\" />http://mock.url.website/questions/3357477/is-as...wed-or-am-i-just-missing-something/3359003#3359003</a>",
+				context.getOutput());
 	}
 
 }

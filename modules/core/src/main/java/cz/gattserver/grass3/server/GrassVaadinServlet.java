e1fd26f74775dbb62f2a6a9fa903ba0d22232a41
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-04-14 20:46:47 +0200

    VaadinSessions requestHandler list init fixed

--- modules/core/src/main/java/cz/gattserver/grass3/server/GrassVaadinServlet.java
+++ modules/core/src/main/java/cz/gattserver/grass3/server/GrassVaadinServlet.java
@@ -1,15 +1,16 @@
 package cz.gattserver.grass3.server;
 
-import java.io.IOException;
 import java.util.Collection;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 import org.springframework.context.ApplicationContext;
 
+import com.vaadin.server.DeploymentConfiguration;
+import com.vaadin.server.ServiceException;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.server.VaadinServletService;
+import com.vaadin.server.VaadinSession;
 
 import cz.gattserver.web.common.spring.SpringContextHelper;
 
@@ -17,29 +18,26 @@ public class GrassVaadinServlet extends VaadinServlet {
 
 	private static final long serialVersionUID = -5411796850116104693L;
 
-	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	private Map<VaadinSession, Boolean> populatedSessions = new WeakHashMap<>();
 
-		/*
-		 * FIX pro registrování requestHandlerů - aktuální implementace
-		 * nefungovala správně, protože registrovala handlery v UI init metodě,
-		 * která se provádí pozdě a navíc registruje handlery opakovaně
-		 */
-		// https://vaadin.com/old-forum/-/message_boards/view_message/3535024
-		// https://vaadin.com/forum#!/thread/3072331
-		// https://vaadin.com/book/vaadin7/-/page/advanced.requesthandler.html
-		getService().addSessionInitListener(event -> {
+	@Override
+	protected VaadinServletService createServletService(DeploymentConfiguration deploymentConfiguration)
+			throws ServiceException {
+		VaadinServletService service = super.createServletService(deploymentConfiguration);
+		service.addSessionInitListener(event -> {
+			VaadinSession session = event.getSession();
+			if (populatedSessions.containsKey(session))
+				return;
 			ApplicationContext context = SpringContextHelper.getContext();
 			if (context != null) {
 				Collection<GrassRequestHandler> grassRequestHandlers = context.getBeansOfType(GrassRequestHandler.class)
 						.values();
 				for (GrassRequestHandler requestHandler : grassRequestHandlers)
 					event.getSession().addRequestHandler(requestHandler);
+				populatedSessions.put(session, Boolean.TRUE);
 			}
 		});
-
-		super.service(request, response);
+		return service;
 	}
 
 }

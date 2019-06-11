ea77a4544d9ed8f6ec9636278d7a90e040860f39
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-06-11 23:31:12 +0200

    Unused Selenium test files removed

--- modules/core/src/test/resources/selenium/seleniumstarthub.sh
+++
@@ -1,1 +0,0 @@
-java -jar selenium-server-standalone-2.30.0.jar -role hub
--- modules/core/src/test/resources/selenium/startnode1.sh
+++
@@ -1,1 +0,0 @@
-java -jar selenium-server-standalone-2.30.0.jar -role node -hub http://localhost:4444/grid/register -maxSession 20 -port 5555 -browser browserName=chrome,maxInstances=20,platform=LINUX

96a8284420839a0dcce9692b0484aafd869266eb
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-06-23 11:09:35 +0200

    Plotter improved; tests

--- modules/contents/plugins/templates/src/test/java/cz/gattserver/grass3/articles/templates/plotter/PlotterParserTest.java
+++ modules/contents/plugins/templates/src/test/java/cz/gattserver/grass3/articles/templates/plotter/PlotterParserTest.java
@@ -38,23 +38,7 @@ public class PlotterParserTest {
 				+ "\" style=\"cursor: pointer; \"></canvas></div>", ctx.getOutput());
 		assertEquals(1, ctx.getJSCodes().size());
 		assertEquals("$.plotter" + validUuid + "=function(){let e,t,l,n,i,o,r=document.getElementById(\"can" + validUuid
-				+ "\"),"
-				+ "a=r.getContext(\"2d\"),c=r.offsetWidth,s=r.offsetHeight,f=!1,h=-5.0,y=6.0,g=-2.0,u=5.0,d=function(x){return 1/x},"
-				+ "m=function(){a.clearRect(0,0,c,s),a.strokeStyle=\"grey\",a.fillStyle=\"grey\";a.font=\"13px Monospace\",a.strokeRect(0,0,c,s);"
-				+ "l=1.01*Math.abs(h-y),n=1.01*Math.abs(g-u),i=s/n,o=c/l;let e=Math.min(h,y),t=Math.max(h,y),r=l/c;if(r<=0||e>t)return void "
-				+ "console.log(\"Plotter error, infinite cycle detected!\");let f=Math.min(g,u),m=(Math.max(g,u),s+Math.min(g,u)*i),M=-Math.min(h,y)*o,"
-				+ "v=8,S=n/v,k=S>10?10*Math.ceil(S/10):Math.ceil(S);((v=Math.ceil(n/k))<1||v==1/0)&&(v=1);let T=Math.floor(f/k)*k;a.setLineDash([5,5]),"
-				+ "a.strokeStyle=\"lightgrey\",a.fillStyle=\"grey\",a.textAlign=\"left\";for(let e=0;e<=v;e++){let t=k*e+T,l=m-t*i;if(l<-21||l>s+21)"
-				+ "continue;a.beginPath(),a.moveTo(0,l),a.lineTo(c,l),a.stroke();let n=l-8;a.fillText(t,M+8,n)}a.setLineDash([]),a.strokeStyle=\"grey\","
-				+ "a.fillStyle=\"grey\",a.beginPath(),a.moveTo(0,m),a.lineTo(c,m),a.stroke();let L=8,x=l/L,b=x>10?10*Math.ceil(x/10):Math.ceil(x);"
-				+ "((L=Math.ceil(l/b))<1||L==1/0)&&(L=1);let E=Math.floor(e/b)*b;a.setLineDash([5,5]),a.strokeStyle=\"lightgrey\",a.fillStyle=\"grey\";"
-				+ "for(let e=0;e<=L;e++){let t=b*e+E,l=M+t*o;if(l<-21||l>c+21)continue;let n=l+8;a.beginPath(),a.moveTo(l,0),a.lineTo(l,s),a.stroke(),"
-				+ "a.textAlign=\"left\",a.fillText(t,n,m-8)}a.setLineDash([]),a.strokeStyle=\"grey\",a.fillStyle=\"grey\",a.beginPath(),a.moveTo(M,0),"
-				+ "a.lineTo(M,s),a.stroke();for(let l=e;l<=t;l+=r){let e=l*o+M,t=m-d(l)*i;a.fillStyle=\"blue\",a.fillRect(e-1.5,t-1.5,3,3)}};"
-				+ "return r.addEventListener(\"wheel\",function(e){e.preventDefault();let t=e.deltaY>0?1.1:1/1.1;h*=t,y*=t,g*=t,u*=t,m();}),"
-				+ "r.addEventListener(\"mousedown\",function(l){f=!0,e=l.clientX,t=l.clientY},!1),r.addEventListener(\"mouseup\",function(e){f=!1},!1),"
-				+ "r.addEventListener(\"mouseleave\",function(e){f=!1},!1),r.addEventListener(\"mousemove\",function(l){if(!f)return;let n=(e-l.clientX)/o,"
-				+ "r=(t-l.clientY)/i;h+=n,y+=n,g-=r,u-=r,m(),e=l.clientX,t=l.clientY},!1),{start:function(){m()}}}(),$.plotter"
+				+ "\"),a=r.getContext(\"2d\"),f=r.offsetWidth,s=r.offsetHeight,c=!1,u=-5.0,h=6.0,y=-2.0,d=5.0,g=u,m=h,v=y,M=d,k=function(x){return 1/x},S=function(e){return Number(e.toPrecision(2))},L=function(){a.clearRect(0,0,f,s),a.strokeStyle=\"grey\",a.fillStyle=\"grey\";a.font=\"13px Monospace\",a.strokeRect(0,0,f,s);l=1.01*Math.abs(u-h),n=1.01*Math.abs(y-d),i=s/n,o=f/l;let e=Math.min(u,h),t=Math.max(u,h),r=l/f;if(r<=0||e>t)return void console.log(\"Plotter error, infinite cycle detected!\");let c=Math.min(y,d),g=(Math.max(y,d),s+Math.min(y,d)*i),m=-Math.min(u,h)*o,v=8,M=S(n/v),L=Math.ceil(n/M);L>v?v++:L<v&&v--,(v<1||v==1/0)&&(v=1);let T=Math.floor(c/M)*M;a.setLineDash([5,5]),a.strokeStyle=\"lightgrey\",a.fillStyle=\"grey\",a.textAlign=\"left\";for(let e=0;e<=v;e++){let t=M*e+T,l=g-t*i;if(l<-21||l>s+21)continue;a.beginPath(),a.moveTo(0,l),a.lineTo(f,l),a.stroke();let n=l-8;a.fillText(S(t),m+8,n)}a.setLineDash([]),a.strokeStyle=\"grey\",a.fillStyle=\"grey\",a.beginPath(),a.moveTo(0,g),a.lineTo(f,g),a.stroke();let b=8,x=S(l/b);Math.ceil(l/x)>b?b++:L<b&&b--,(b<1||b==1/0)&&(b=1);let E=Math.floor(e/x)*x;a.setLineDash([5,5]),a.strokeStyle=\"lightgrey\",a.fillStyle=\"grey\";for(let e=0;e<=b;e++){let t=x*e+E,l=m+t*o;if(l<-21||l>f+21)continue;let n=l+8;a.beginPath(),a.moveTo(l,0),a.lineTo(l,s),a.stroke(),a.textAlign=\"left\",a.fillText(S(t),n,g-8)}a.setLineDash([]),a.strokeStyle=\"grey\",a.fillStyle=\"grey\",a.beginPath(),a.moveTo(m,0),a.lineTo(m,s),a.stroke();for(let l=e;l<=t;l+=r){let e=l*o+m,t=g-k(l)*i;a.fillStyle=\"blue\",a.fillRect(e-1.5,t-1.5,3,3)}};return r.addEventListener(\"wheel\",function(e){e.preventDefault();let t=e.deltaY>0?1.1:1/1.1;u*=t,h*=t,y*=t,d*=t,L()}),r.addEventListener(\"mousedown\",function(l){c=!0,e=l.clientX,t=l.clientY},!1),r.addEventListener(\"mouseup\",function(e){c=!1},!1),r.addEventListener(\"mouseleave\",function(e){c=!1},!1),r.addEventListener(\"mousemove\",function(l){if(!c)return;let n=(e-l.clientX)/o,r=(t-l.clientY)/i;u+=n,h+=n,y-=r,d-=r,L(),e=l.clientX,t=l.clientY},!1),r.addEventListener(\"dblclick\",function(e){u=g,h=m,y=v,d=M,L()},!1),{start:function(){L()}}}(),$.plotter"
 				+ validUuid + ".start();", ctx.getJSCodes().iterator().next());
 	}
 

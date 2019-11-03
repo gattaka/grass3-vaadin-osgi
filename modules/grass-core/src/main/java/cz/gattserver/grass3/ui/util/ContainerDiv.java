73dabc4330c78107ea0326cba425bd3754ac02ec
Hynek Uhlíř <hynek.uhlir@gmail.com>
2019-11-03 20:24:50 +0100

    Tab-size fixed

--- modules/grass-core/src/main/java/cz/gattserver/grass3/ui/util/ContainerDiv.java
+++ modules/grass-core/src/main/java/cz/gattserver/grass3/ui/util/ContainerDiv.java
@@ -9,7 +9,8 @@ public class ContainerDiv extends Div {
 	public ContainerDiv() {
 		getStyle().set("padding", "var(--lumo-space-m)").set("border", "1px solid lightgray")
 				.set("border-radius", "var(--lumo-border-radius-m)").set("overflow-y", "auto")
-				.set("white-space", "pre-wrap").set("font-family", "monospace").set("font-size", "10pt");
+				.set("white-space", "pre-wrap").set("font-family", "monospace").set("font-size", "10pt")
+				.set("tab-size", "4");
 	}
 
 }
